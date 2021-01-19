f9bf8620814b72ccd3ccf26c06708a4d45de96e6
captain guliye <guliye91@gmail.com>
2021-01-19 21:56:06 +0300

    added required js and header css

--- main.js
+++ main.js
@@ -2,9 +2,7 @@ const burgerBtn = document.querySelector('.burger');
 const closeBtn = document.querySelector('.close');
 const menu = document.querySelector('.main-nav');
 
-burgerBtn.addEventListener('click', ()=>{
+
+burgerBtn.addEventListener('click',()=>{
     menu.classList.toggle('show');
-    closeBtn.classList.add('show-close');
 })
-
-
