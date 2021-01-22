589561fcfeedbbdffba3528074d773ee293a894c
captain guliye <guliye91@gmail.com>
2021-01-22 06:35:44 +0300

    updated media queries to make the site responsive

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 const burgerBtn = document.querySelector('.burger');
 const closeBtn = document.querySelector('.close');
 const menu = document.querySelector('.main-nav');
+const links = menu.querySelectorAll('.main-nav li a');
 
+links.forEach((link)=>{
+    link.addEventListener('click',()=>{
+        menu.classList.remove('show');
+    })
+})
 
 burgerBtn.addEventListener('click',()=>{
     menu.classList.toggle('show');
